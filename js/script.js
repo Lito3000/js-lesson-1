97ffb5f8dd76fee1ae8a03ab646b1fe502b5ba4c
Lito3000 <acersytec@i.ua>
2023-10-19 11:02:56 +0300

    HomeWorkCV

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
+document.querySelector('#link').addEventListener('click', newTweet);
 
 
+function newTweet(ej) {
+    ej.preventDefault();
+    list.innerHTML = `<img src="${(src="./img/avataaars1.png")}" alt="No Pictures" width="250px" height="250px">`;
+}
 
 
 
