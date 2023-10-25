0fd1e3dae464b5d7e7e1dffeba620f9c1fa1fd8e
Lito3000 <acersytec@i.ua>
2023-10-25 10:01:05 +0300

    work4

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ var i=0;
 var image=document.getElementById("image");
 // Добавьте свои картинки в массив через запятую
 var imgs=new Array('./img/avataaars1.png',
-    './img/avataaars.png', './img/avataaars2.png','./img/avataaars3.png');
+    './img/avataaars.png', './img/avataaars3.png','./img/avataaars2.png');
 function imgsrc() {
     i++;
     i = i % imgs.length;
